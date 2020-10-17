e9118f461609e8eb1efc01236d8cf56428c5fb48
David Anderson <david@pibcak.net>
2020-10-17 15:27:38 +0100

    Updated the addition of the git repo

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -32,6 +32,7 @@ if [[ "{{cookiecutter.version_control}}" == "git" ]]; then
         "$GIT" remote add origin "{{cookiecutter.version_control_remote_origin}}"
         "$GIT" pull --quiet --rebase origin master
         "$GIT" push --quiet origin master
+        "$GIT" branch --set-upstream-to=origin/master master
     else
         echo "WARNING: git requested, but git binary not found" >&2
     fi
